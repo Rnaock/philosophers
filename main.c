983f7fa4c7f91787fd84a23e3aee5571840f7fce
Manuel BIMICH <mabimich@e1r7p4.clusters.42paris.fr>
2022-08-30 20:06:16 +0200

    wip

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mabimich <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 18:16:08 by mabimich          #+#    #+#             */
-/*   Updated: 2022/08/29 20:38:58 by mabimich         ###   ########.fr       */
+/*   Updated: 2022/08/30 19:13:48 by mabimich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,16 @@
 # define NC	"\e[0m"
 # define YELLOW	"\e[1;33m"
 
-void	*thd_routine(void *d)
+time_t	get_time_in_ms(void)
+{
+	struct timeval	tv;
+
+	if (gettimeofday(&tv, NULL))
+		return (0);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+void	*philo_routine(void *d)
 {
 	pthread_t tid;
 
@@ -25,25 +34,35 @@ void	*thd_routine(void *d)
 	return (NULL);
 }
 
-t_data	*create_thd(t_data *data)
+t_data	*create_philo(t_data *data)
 {
 	int	i;
 	int	out;
 
 	i = -1;
 	out = 0;
+	init_philo(data);
 	while (++i < data->n[0] && !out)
 	{
-		out = pthread_create(&data->thd[i], NULL, thd_routine, data);
-		printf("%d) Thread [%ld]: %d\n", i, data->thd[i], out);
+		printf("%d) Thread [%ld]: %d\n", i, data->philo[i]->thd, out);
+		out = pthread_create(&data->philo[i]->thd, NULL, philo_routine, data);
 	}
 	i = -1;
 	while (++i < data->n[0] && !out)
 	{
-		printf("%d) join [%ld]\n", i, data->thd[i]);
-		out = pthread_join(data->thd[i], NULL);
+		printf("%d) join [%ld]\n", i, data->philo[i]->thd);
+		out = pthread_join(data->philo[i]->thd, NULL);
 	}
 	return (data);
+}
+
+void	init_philo(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	while (i < n[0])
+		data->philo[i]->id = i;
 }
 
 t_data	*init(int ac, char **av)
@@ -65,22 +84,22 @@ t_data	*init(int ac, char **av)
 			return (NULL);
 		}
 	}
-	data->thd = ft_calloc(sizeof(pthread_t), data->n[0]);
-	if (!data->thd)
+	data->philo = ft_calloc(sizeof(t_philo), data->n[0]);
+	if (!data->philo)
 	{
 		free(data);
 		ft_putendl_fd("Erreur malloc", 2);
 		return (NULL);
-	}
 	data->fork = ft_calloc(sizeof(t_fork), data->n[0]);
 	if (!data->fork)
 	{
-		free(data->thd);
+		free(data->philo);
 		free(data);
 		ft_putendl_fd("Erreur malloc", 2);
 		return (NULL);
 	}
-	return (create_thd(data));
+	data->start_t = get_time_in_ms();
+	return (data);
 }
 
 int	main(int ac, char **av)
@@ -93,8 +112,9 @@ int	main(int ac, char **av)
 	if (ac != 5 && ac != 6)
 		return (ft_putendl_fd("Usage: 'number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat", 2), 1);
 	data = init(ac, av);
-	if (!data)
+	if (!data || !data->start_t)
 		return (1);
+	create_philo(data);
 	free(data);
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: mabimich <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 18:25:49 by mabimich          #+#    #+#             */
-/*   Updated: 2022/08/29 20:36:49 by mabimich         ###   ########.fr       */
+/*   Updated: 2022/08/30 17:31:43 by mabimich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,37 @@
 
 # include "libft/includes/libft.h"
 # include <sys/wait.h>
+# include <sys/time.h>
 # include <stdio.h>
 # include <pthread.h>
 
 typedef	struct s_fork
 {
-	pthread_mutex_t	count_mutex;
-	unsigned int	count;
-}			 		t_fork;
+	pthread_mutex_t	fork_mtx;
+	size_t			in_use;
+}	t_fork;
+
+typedef struct s_philo
+{
+	int			id;
+	int			n[5];
+	pthread_t	*thd;
+	t_fork		*fork;
+	time_t		start_t;
+	int			finish;
+	int			n_of_philo;
+	int			t_2_die;
+	int			t_2_eat;
+	int			t_2_sleep;
+	int			n_of_t_philo_eat;
+}				t_philo;
 
 typedef struct s_data
 {
 	int			n[5];
-	pthread_t	*thd;
-	t_fork		*fork
+	t_fork		*fork;
+	t_philo		*philo;
+	time_t		start_t;
 	int			finish;
 	int			n_of_philo;
 	int			t_2_die;
